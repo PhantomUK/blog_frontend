6fd5408e00f250e2c3486513e1c66fb7133da9fa
Tim <tim@lowton.org>
2015-08-05 17:04:02 +0100

    Pulling in articles

--- BlogListController.js
+++ BlogListController.js
@@ -3,14 +3,29 @@
 
     var blog = angular.module('myblog');
 
-    blog.controller('BlogListController', ['$scope', 'blogAPI', function ($scope, blogAPI) {
+    blog.controller('BlogListController', ['$scope', '$sce', 'blogAPI', function ($scope, $sce, blogAPI) {
 
         var onBlogListComplete = function(data) {
             $scope.blog_list = data;
         };
 
+        var onBlogArticleComplete = function(data) {
+            $scope.blog_article = data;
+        };
+
         blogAPI.getBlogList().then(onBlogListComplete);
+
+        $scope.getNid = function(nodeItem) {
+            return blogAPI.getBlogArticle(nodeItem.nid).then(onBlogArticleComplete);
+        }
+
+        return {
+             getNid: $scope.getNid
+        }
 
     }]);
 
 })();
+
+
+//$sce.trustAsHtml(
--- BlogListService.js
+++ BlogListService.js
@@ -4,12 +4,21 @@
     var blogAPI = function($http){
         var getBlogList = function() {
             return $http.get("http://timblog-culcheth.rhcloud.com/apiv1/node/").then(function (response) {
+                console.log(response.data);
+                return response.data;
+            });
+        };
+
+        var getBlogArticle = function(nid) {
+            return $http.get("http://timblog-culcheth.rhcloud.com/apiv1/node/" + nid).then(function (response) {
+                console.log(response.data);
                 return response.data;
             });
         };
 
         return {
-            getBlogList: getBlogList
+            getBlogList: getBlogList,
+            getBlogArticle: getBlogArticle
         };
     };
 
